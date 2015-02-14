51fef4dce75d937456e048ae951a6ca2f4d98cd4
Prelang Builder <builder@prelang.com>
2015-02-14 12:20:48 +0000

    Created an Omniauth callback route to point to the Omniauth callback controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
